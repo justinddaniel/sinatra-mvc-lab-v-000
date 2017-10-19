e6d645c3e9a9152b968b682c425d4b753ae96374
Justin Daniel <justinddaniel@gmail.com>
2017-10-19 22:48:28 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,23 +1,57 @@
 class PigLatinizer
-  attr_accessor :phrase
 
-  def initialize(phrase = nil)
-    @phrase = phrase
+  def piglatinize(word)
+
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u"]
+
+    if non_pig_latin_words.include?(word)
+      word
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      while !vowels.include?(word[0])
+        consonants << word[0]
+        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
+    end
   end
 
-  def piglatinize(phrase)
-    phrase_array = phrase.split(" ")
-    phrase_array.map! do |w|
-      if w.chars.first.match(/[AaEeIiOoUu]/) != nil
-        "#{w}way"
-      else
-        wn = w.reverse
-        wnp = wn[0..-2].reverse + wn[-1]
-        "#{wnp}ay"
-      end
-    end
-    phrase_to_print = phrase_array.join(" ")
-    "#{phrase_to_print}"
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
   end
 
 end
+
+p = PigLatinizer.new
+
+puts p.piglatinize("pork")
+
+#my solution that works but is not speced to tests. 
+
+#class PigLatinizer
+  #attr_accessor :phrase
+
+#  def initialize(phrase = nil)
+  #  @phrase = phrase
+  #end
+
+  #def piglatinize(phrase)
+    #phrase_array = phrase.split(" ")
+    #phrase_array.map! do |w|
+      #if w.chars.first.match(/[AaEeIiOoUu]/) != nil
+      #  "#{w}way"
+    #  else
+      #  wn = w.reverse
+      #  wnp = wn[0..-2].reverse + wn[-1]
+      #  "#{wnp}ay"
+    #  end
+  #  end
+  #  phrase_to_print = phrase_array.join(" ")
+  #  "#{phrase_to_print}"
+#  end
+
+#end
