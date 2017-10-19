19c4af4deea288652bdfbb1736987163b9816c8a
Justin Daniel <justinddaniel@gmail.com>
2017-10-19 22:13:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,15 @@
-require 'pry'
 class Piglatinizer
   attr_reader :phrase
 
   def initialize(phrase)
     phrase_array = phrase.split(" ")
     phrase_array.map do |w|
-      binding.pry
+      if w.chars.first.match(/[AaEeIiOoUu]) != nil
+        "#{w}way"
+      else
+        wn = w.reverse
+        wnp = wn[0..-2].reverse + wn[-1]
+        "#{wnp}ay"
     end
 
   end
