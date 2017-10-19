9b0c6a6393df63261233930d4d337b206fc590dc
Justin Daniel <justinddaniel@gmail.com>
2017-10-19 22:40:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
 
     phrase_from_user = params[:user_phrase]
-    @phrase_from_user = Piglatinizer.new(phrase_from_user)
+    @phrase_from_user = PigLatinizer.new(phrase_from_user)
 
     erb :piglatinize
   end
