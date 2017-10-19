7f6798d11888384bc3f9414616f45f96b9bda80a
Justin Daniel <justinddaniel@gmail.com>
2017-10-19 23:03:40 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,7 @@ class PigLatinizer
     #if non_pig_latin_words.include?(word)
       #word
     if vowels.include? word[0]
-      word << "ay"
+      word << "way"
     else
       consonants = ""
       while !vowels.include?(word[0])
